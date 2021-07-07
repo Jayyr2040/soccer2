f40afff8c07635dbe56920301b091d11ef55fe83
jaytai2040 <jayyr2040@gmail.com>
2021-07-07 18:58:28 +0800

    0707U6

--- src/FullNews.js
+++ src/FullNews.js
@@ -4,9 +4,9 @@ import newsData from './staticData';
 
 function FullNews(props) {
     console.log("Full News Coverage",props.location.data?.props.news);
-    console.log("Fake full news coverage", newsData.news);
+    console.log("Fake full news coverage", newsData[(props.location.data?.props.count)].news);
     window.scrollTo(0, 0)
-    const cNewsData = (props.location.data?.props.news.articles.length > 0? props.location.data?.props.news :newsData[props.location.data?.props.count].news)  ;
+    const cNewsData = ((props.location.data?.props?.news.articles.length > 0) ? props.location.data?.props.news :newsData[props.location.data?.props.count].news)  ;
 
     let list = (cNewsData.articles).map((item,index) => {
         return (
--- src/WebUrl.js
+++ src/WebUrl.js
@@ -1,5 +1,5 @@
 const WebUrl = {
-    headlines: `https://gnews.io/api/v4/top-headlines?token=${process.env.REACT_APP__GNEWS_API_KEY5}&lang=en&max=2&in=title&q=english+premier+league`,
+    headlines: `https://gnews.io/api/v4/top-headlines?token=${process.env.REACT_APP__GNEWS_API_KEY6}&lang=en&max=2&in=title&q=english+premier+league`,
     search: `https://gnews.io/api/v4/search?token=${process.env.REACT_APP__GNEWS_API_KEY3}&lang=en&max=2&in=title&in=description&q=`,
     favourites: `https://gnews.io/api/v4/search?token=${process.env.REACT_APP__GNEWS_API_KEY4}&lang=en&max=2&in=title&q=fabrizio+romano`,
     highlights: 'https://www.scorebat.com/video-api/v1/'
